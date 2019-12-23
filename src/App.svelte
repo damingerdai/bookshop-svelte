c6bf318c81f42e41ee4e40e11450a52ec9ed06da
明国宾 <mingguobin@live.com>
2019-12-23 19:05:19 +0800

    init: init the bookshop

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	import BookCard from './BookCard.svelte';
+	export let name;
+	let title = '';
+	let price = 0;
+	let description = '';
+
+	let books = [];
+	function handleAddBook() {
+		books = books.concat({
+			title,
+			price,
+			description
+		});
+		title = '';
+		price = 0;
+		description = '';
+	}
+</script>
+
+<main>
+	<h1>Welcome to {name}'s online bookstore!</h1>
+	<section>
+		<h2>Add new book</h2>
+		<label for="title">Title</label>
+		<input type="text" id="title" bind:value={title}>
+		<label for="price">price</label>
+		<input type="number" id="price" bind:value={price}>
+		<label for="description">Description</label>
+		<textarea name="description" id="description" rows="3" bind:value={description}></textarea>
+		<div>
+			<button on:click={handleAddBook}>add book</button>
+		</div>
+		{#each books as book} 
+			<BookCard title={book.title} price={book.price} description={book.description} />
+		{/each}
+		
+	</section>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/BookCard.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let title;
+    export let price;
+    export let description;
+</script>
+
+<div class="card">
+    <h1>{title}</h1>
+    <h2>{price}</h2>
+    <p>{description}</p>
+    <button>添加购物车</button>
+</div>
+
+<style>
+    .card {
+        text-align: left;
+        border-radius: 5px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26)
+    }
+
+    h1 {
+        font-size: 20px;
+    }
+
+    h2 {
+        font-size: 15px;
+        color: #999;
+    }
+
+    button {
+        background-color: #ff3e00;
+        border-radius: 5px;
+        border: none;
+        color: white;
+    }
+</style>
